62ab2ff8a1f80663e2270b0107e1376c83801df2
Júlia Rayssa Hintz <32268701+juliahintz@users.noreply.github.com>
2017-10-10 09:35:25 -0300

    1aula- projeto 3 tri

---
+++ Trab.3tri/professores.php
@@ -0,0 +1,56 @@
+<?php
+
+	//contém funções para acesso e manipulação dos dados de professores
+
+	function buscaProfessor($codigo){
+		$professor = array();
+
+	//abrir arquivo
+
+		$dados = file("dados/professores.csv");
+
+	//percorrer
+
+		foreach ($dados as $linha) {
+			$colunas = explode("," , $linha);
+			if ($colunas[0]==$codigo){
+				$professor['siape'] = $colunas[0];
+				$professor['nome'] = $colunas[1];
+				$professor['email'] = $colunas[2];
+				$professor['foto'] = $colunas[3];
+			}
+		}
+
+
+		return $professor;
+
+	}
+
+
+
+	function listaProfessores(){
+
+		$professores = array();
+
+		//abrir arquivo
+		$dados = file("dados/professores.csv");
+
+		//percorrer
+		foreach ($dados as $linha) {
+			$colunas = explode("," , $linha);
+
+			$professor['siape'] = $colunas[0];
+			$professor['nome'] = $colunas[1];
+			$professor['email'] = $colunas[2];
+			$professor['foto'] = $colunas[3];
+
+			$professores[] = $professor;
+		}
+
+
+		return $professores;
+
+	}
+
+	
+	
---
+++ Trab.3tri/testes/testaBuscaProfessor.php
@@ -0,0 +1,7 @@
+<?php
+
+
+	$lista = listaProfessores();
+
+	print_r($lista);
+
