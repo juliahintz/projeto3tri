c7ba9adafefe04695c4c769801a9cc4d48cd42eb
Júlia Rayssa Hintz <32268701+juliahintz@users.noreply.github.com>
2017-10-31 09:29:38 -0200

    aula dia 31/10

--- Trab.3tri/detalhaAluno.php
+++ Trab.3tri/detalhaAluno.php
@@ -1,4 +1,4 @@
-<?php /*
+<?php
 
 	include("cabecalho.php");
 
@@ -7,12 +7,11 @@
 	//recupera o cod enviado via metodo GET
 	$matricula = $_GET['cod'];
 
+
 	//chama função, passando o codigo do professor
 	$aluno = buscaAluno($matricula);
-
-?>
 	
-
+?>
 	<!-- colunas para centralizar -->
 	<div class="coluna10">.</div>
 
@@ -26,53 +25,11 @@
 
 	<section class="dados">
 		<h2><?=$aluno['nome']?></h2>
-			<h3><?=$aluno['email']?></h3>
-			
+		<h3><?=$aluno['email']?></h3>	
 	</section>
 
 
 <?php
-
-	include("rodape.php")
-
-*/
-?>
-
-<?php
-
-	include("cabecalho.php");
-
-	include("alunos.php");
-
-	//recupera o cod enviado via metodo GET
-	$matricula = $_GET['cod'];
-
-	//chama função, passando o codigo do professor
-	$aluno = buscaAluno($matricula);
-
-?>
-	
-
-	<!-- colunas para centralizar -->
-	<div class="coluna10">.</div>
-
-	<!-- conteudo principal -->
-	<article class="coluna80">
-
-
-	<section class="foto">
-		<img src="<?=$aluno['foto'] ?>">
-	</section>
-
-	<section class="dados">
-		<h2><?=$aluno['nome']?></h2>
-		<h3><?=$aluno['email']?></h3>
-			
-	</section>
-
-
-<?php
-
 	include("rodape.php")
 
 ?>
